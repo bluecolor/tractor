5bd05505bf6e33a189513744a884ed7d2a1008df
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2022-01-25 16:02:26 +0300

    mysql input

--- pkg/lib/connectors/mysql/mysql_input.go
+++ pkg/lib/connectors/mysql/mysql_input.go
@@ -1,10 +1,92 @@
 package mysql
 
 import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/bluecolor/tractor/pkg/lib/feed"
 	"github.com/bluecolor/tractor/pkg/lib/meta"
 	"github.com/bluecolor/tractor/pkg/lib/wire"
+	"github.com/rs/zerolog/log"
 )
 
-func (m *MySQLConnector) Read(e meta.ExtInput, w wire.Wire) error {
-	return nil
+func (m *MySQLConnector) StartWorker(e meta.ExtInput, w wire.Wire, i int) (err error) {
+	query, err := m.BuildQuery(e, i)
+	if err != nil {
+		return err
+	}
+	rows, err := m.db.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	bufferSize := e.Config.GetInt("buffer_size", 100)
+	buffer := []feed.Record{}
+	for rows.Next() {
+		columns := make([]interface{}, len(e.Fields))
+		columnPointers := make([]interface{}, len(e.Fields))
+		for i := range columns {
+			columnPointers[i] = &columns[i]
+		}
+		if err := rows.Scan(columnPointers...); err != nil {
+			return err
+		}
+		record := feed.Record{}
+		for i, f := range e.Fields {
+			if f.Name == "" {
+				f.Name = fmt.Sprintf("col%d", i)
+			}
+			record[f.Name] = columns[i]
+		}
+		if len(buffer) >= bufferSize {
+			w.SendData(buffer)
+			w.SendFeed(feed.NewReadProgress(len(buffer)))
+			buffer = []feed.Record{}
+		} else {
+			buffer = append(buffer, record)
+		}
+	}
+	if len(buffer) > 0 {
+		w.SendData(buffer)
+		w.SendFeed(feed.NewReadProgress(len(buffer)))
+	}
+	return
+}
+
+func (m *MySQLConnector) Read(e meta.ExtInput, w wire.Wire) (err error) {
+	var parallel int = 1
+	if e.Parallel > 1 {
+		log.Warn().Msgf("parallel is not supported for MySQL connector. Using %d", parallel)
+	}
+	if e.Parallel < 1 {
+		log.Warn().Msgf("invalid parallel setting %d. Using %d", e.Parallel, parallel)
+	}
+	wg := &sync.WaitGroup{}
+	for i := 0; i < parallel; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, i int) {
+			defer wg.Done()
+			err := m.StartWorker(e, w, i)
+			if err != nil {
+				w.SendFeed(feed.NewErrorFeed(feed.SenderInputPlugin, err))
+			}
+		}(wg, i)
+	}
+	return
+}
+
+func (m *MySQLConnector) BuildQuery(e meta.ExtInput, i int) (query string, err error) {
+	if e.Fields == nil || len(e.Fields) == 0 {
+		return "", fmt.Errorf("no fields specified")
+	}
+	columns := ""
+	for _, f := range e.Fields {
+		columns += f.Name + ",\n"
+	}
+	columns = strings.TrimRight(columns, ",\n")
+	query = fmt.Sprintf("select\n%s\nfrom %s", columns, e.Dataset.Name)
+	log.Debug().Msgf("query: %s", query)
+	return
 }
--- pkg/lib/meta/meta.go
+++ pkg/lib/meta/meta.go
@@ -1,30 +1,45 @@
 package meta
 
+type Config map[string]interface{}
+
+func (c Config) GetString(key string, def string) string {
+	if v, ok := c[key]; ok {
+		if s, ok := v.(string); ok {
+			return s
+		}
+	}
+	return def
+}
+func (c Config) GetInt(key string, def int) int {
+	if v, ok := c[key]; ok {
+		if i, ok := v.(int); ok {
+			return i
+		}
+	}
+	return def
+}
+
 type Dataset struct {
-	Name   string                 `json:"name"`
-	Config map[string]interface{} `json:"config"`
-	Fields []Field                `json:"fields"`
+	Name   string  `json:"name"`
+	Config Config  `json:"config"`
+	Fields []Field `json:"fields"`
 }
-
 type Field struct {
-	Name   string                 `json:"name"`
-	Type   string                 `json:"type"`
-	Config map[string]interface{} `json:"config"`
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Config Config `json:"config"`
 }
-
 type ExtInput struct {
 	Dataset
 	Parallel int `json:"parallel"`
 }
-
 type ExtOutput struct {
 	Dataset
 	Parallel      int            `json:"parallel"`
 	FieldMappings []FieldMapping `json:"fieldMappings"`
 }
-
 type FieldMapping struct {
-	SourceField Field                  `json:"sourceField"`
-	TargetField Field                  `json:"targetField"`
-	Config      map[string]interface{} `json:"config"`
+	SourceField Field  `json:"sourceField"`
+	TargetField Field  `json:"targetField"`
+	Config      Config `json:"config"`
 }
