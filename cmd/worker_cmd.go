06b86cc87e01cd6bb60acd1251f2d17b65b170f5
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2022-02-12 13:37:21 +0300

    worker run cmd

---
+++ cmd/worker_cmd.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"github.com/bluecolor/tractor/pkg/tasks"
+	"github.com/urfave/cli/v2"
+)
+
+func runWorkerStartCmd(c *cli.Context) error {
+	return tasks.NewWorker(config.Tasks).Run()
+}
